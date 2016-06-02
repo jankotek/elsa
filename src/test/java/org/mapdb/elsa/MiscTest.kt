734c924159679cbfce3ce79be4f485bb1a4a7943
Jan Kotek <jan@kotek.net>
2016-06-02 09:29:30 +0300

    Externalizable  test

--- src/test/java/org/mapdb/elsa/MiscTest.kt
+++ src/test/java/org/mapdb/elsa/MiscTest.kt
@@ -2,6 +2,9 @@ package org.mapdb.elsa
 
 import org.junit.Test
 import org.junit.Assert.*
+import java.io.Externalizable
+import java.io.ObjectInput
+import java.io.ObjectOutput
 import java.util.*
 import java.util.Arrays.asList
 
@@ -17,5 +20,55 @@ class MiscTest{
         clone(asList<Any>("aa","bb"))
     }
 
+
+    class Extern2: Externalizable {
+
+        companion object{
+            var readCount = 0
+            var writeCount = 0
+        }
+
+        var notSet:Any? =null
+
+        var field:String = ""
+
+        var readCountHere = 0
+        var writeCountHere = 0
+
+        override fun readExternal(oi: ObjectInput) {
+            readCount++
+            readCountHere++
+            field = oi.readUTF()
+        }
+
+        override fun writeExternal(out: ObjectOutput) {
+            writeCount++
+            writeCountHere++
+            out.writeUTF(field)
+        }
+
+    }
+
+    @Test fun externalizableUsed(){
+        assertEquals(0,Extern2.readCount)
+        assertEquals(0,Extern2.writeCount)
+
+        val e = Extern2()
+        e.notSet = "ignore this"
+        e.field = "dont ignore"
+        val e2 = SerializerBaseTest.clonePojo(e)
+
+        assertEquals(0, e.readCountHere)
+        assertEquals(1, e.writeCountHere)
+
+        assertEquals(1,Extern2.readCount)
+        assertEquals(1,Extern2.writeCount)
+
+        assertEquals(1, e2.readCountHere)
+        assertEquals(0, e2.writeCountHere)
+
+        assertNull(e2.notSet)
+        assertEquals("dont ignore", e.field)
+    }
 
 }
